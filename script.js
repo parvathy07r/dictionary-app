e00b7f7c4adbb93fdde7aa91d68486605ff2ac10
Parvathy R <parvathy07r@gmail.com>
2024-07-30 16:31:59 +0530

    feat: added code to display meanings

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 const inputElement = document.querySelector("input");
 //select search button
 const searchButton = document.querySelector("button");
+//select result section
+const resultSection = document.querySelector(".result-section");
+//select section to display nouns 
+const nounSection = document.querySelector(".nouns");
+//select section to display verbs
+const verbSection = document.querySelector(".verbs");
 
 //add eventlistener to the search button
 searchButton.addEventListener("click", (event) => {
@@ -10,16 +16,56 @@ searchButton.addEventListener("click", (event) => {
     //extract input value and store it 
     const searchQuery = inputElement.value;
 
+    nounSection.innerHTML = "";
+    verbSection.innerHTML = "";
+
     //make get request to the url
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchQuery}`)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            data[0].meanings.forEach((meaning) => {
+                if (meaning.partOfSpeech === 'noun') {
+                    nounSection.innerHTML += `
+                <div class="noun-display">
+                        <span class="word">${searchQuery}</span>
+                        <ol>
+                            ${meaning.definitions.map((definition) =>
+                        `<li>${definition.definition}</li>`
+                    ).join('')}
+                        </ol>
+                </div>
+                `
+
+                }
+
+                if (meaning.partOfSpeech === 'verb') {
+                    verbSection.innerHTML += `
+                <div class="verb-display">
+                        <span class="word">${searchQuery}</span>
+                        <ol>
+                            ${meaning.definitions.map((definition) =>
+                        `<li>${definition.definition}</li>`
+                    ).join('')}
+                        </ol>
+                </div>
+                `
+                }
+
+
+
+
+
+
+            })
+
+
+        })
+        .catch((error) => {
+            console.error(error);
+        });
 });
+
 
