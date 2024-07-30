e3ffe9d2ff9eea70492152858101c07c140febfa
Parvathy R <parvathy07r@gmail.com>
2024-07-30 12:42:01 +0530

    feat: added code to make get request to the url

--- script.js
+++ script.js
@@ -6,5 +6,20 @@ const searchButton = document.querySelector("button");
 //add eventlistener to the search button
 searchButton.addEventListener("click", (event) => {
     console.log(inputElement.value);
+
+    //extract input value and store it 
+    const searchQuery = inputElement.value;
+
+    //make get request to the url
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchQuery}`)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
 });
 
